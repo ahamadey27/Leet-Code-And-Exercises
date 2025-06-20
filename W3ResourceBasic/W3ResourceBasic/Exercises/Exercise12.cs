f10982bb8d439a7e176254a2426bbeb639b84730
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-20 09:43:19 -0400

    added comments in program

--- W3ResourceBasic/W3ResourceBasic/Exercises/Exercise12.cs
+++ W3ResourceBasic/W3ResourceBasic/Exercises/Exercise12.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace W3ResourceBasic.Exercises
 {
-    //multiply the corresponding elements of two integer arrays
+    //Multiply the corresponding elements of two integer arrays
     public static class Exercise12
     {
         public static void Run()
@@ -14,6 +14,24 @@ namespace W3ResourceBasic.Exercises
             Console.WriteLine("Exercise 12");
             Console.WriteLine("Multiply the corresponding elements of two integer arrays");
             Console.WriteLine("Enter four elements for first array");
+
+            int[] arrayOne = new int[3];
+            int[] arrayTwo = new int[3];
+
+            for (int i = 0; i < arrayOne.Length; i++)
+            {
+                Console.WriteLine($"Enter number {i + 1}");
+                arrayOne[i] = int.Parse(Console.ReadLine()); // Fix: Convert string input to integer
+            }
+
+            Console.WriteLine("Enter four elements for second array");
+
+            for (int i = 0; i < arrayTwo.Length; i++)
+            {
+                Console.WriteLine($"Enter number {i + 1}");
+                arrayTwo[i] = int.Parse(Console.ReadLine()); // Fix: Convert string input to integer
+            }
+
 
         }
     }
