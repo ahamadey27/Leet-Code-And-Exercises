be452027c0cfcbdc1e7b84c192810c84d28aa779
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-27 13:39:44 -0400

    exercise 23

---
+++ W3ResourceBasic/W3ResourceBasic/Exercises/Exercise23.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3ResourceBasic.Exercises
+{
+    public static class Exercise23
+    {
+        public static void Run()
+        {
+            string? phpString;
+            Console.WriteLine("Exercise 23");
+            Console.WriteLine("Check if \"HP\" appears at second position in a string and returns the string without \"HP\"");
+            Console.Write("Enter PHP Tutorial.");
+            do
+            {
+                phpString = Console.ReadLine();
+
+                if (phpString != "PHP Tutorial")
+                {
+                    Console.WriteLine("Please try again");
+                }
+            } while (false);
+
+            // Check if the substring from index 1 of length 2 in 'str' equals "HP"
+            // If true, remove the substring "HP" starting from index 1; otherwise, keep the original string 'str'
+            Console.WriteLine((phpString.Substring(1, 2).Equals("HP") ? phpString.Remove(1, 2) : phpString));
+        }
+    }
+}
