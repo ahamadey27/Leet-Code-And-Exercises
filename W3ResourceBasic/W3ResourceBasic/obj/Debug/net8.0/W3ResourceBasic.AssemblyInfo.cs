5f274dfd21a1b6a8ba3f0a0ef9c6b5473faa3158
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-06-30 10:50:13 -0400

    Update project structure and versioning information
    - Added new document entry for `program.cs` in `DocumentLayout.backup.json`.
    - Updated `SelectedChildIndex` from 13 to 7.
    - Changed `DocumentIndex` for `MiniProj01.cs` from 1 to 0 and for `Program.cs` from 0 to 1.
    - Modified `ViewState` for `MiniProj01.cs`.
    - Updated `AssemblyInformationalVersionAttribute` in `W3ResourceBasic.AssemblyInfo.cs`.
    - Reflected new commit hash in `W3ResourceBasic.sourcelink.json`.
    - Rebuilt several binary files including `W3ResourceBasic.dll`, `W3ResourceBasic.exe`, and `W3ResourceBasic.pdb`.

--- W3ResourceBasic/W3ResourceBasic/obj/Debug/net8.0/W3ResourceBasic.AssemblyInfo.cs
+++ W3ResourceBasic/W3ResourceBasic/obj/Debug/net8.0/W3ResourceBasic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("W3ResourceBasic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dea465762982351624b45f4b1c880822c30e1ce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beed2f42f1eca03c19e08b72d479f2c8dc2e8d08")]
 [assembly: System.Reflection.AssemblyProductAttribute("W3ResourceBasic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("W3ResourceBasic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
